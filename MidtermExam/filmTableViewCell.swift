9cea175db2aaa4a14da60d1a3160289aaf9be7c0
John <priority918@gmail.com>
2016-09-02 17:07:40 +0800

    first commit

---
+++ MidtermExam/filmTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  filmTableViewCell.swift
+//  MidtermExam
+//
+//  Created by 阿銘 on 2016/9/2.
+//  Copyright © 2016年 UI. All rights reserved.
+//
+
+import UIKit
+
+class filmTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
